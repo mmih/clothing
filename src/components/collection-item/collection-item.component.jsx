85e918f942f22356f5cd15fa8d048cfd046f12d5
joelynnturner@gmail.com <joelynnturner@gmail.com>
2020-01-11 18:02:31 +0200

    - cart data updated  - add collection items to cart  - group collection items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, price, name, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+    const{ name, price, imageUrl} = item;
+    return(
     <div className='collection-item'>
         <div 
             className='image'
@@ -14,7 +20,12 @@ const CollectionItem = ({id, price, name, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton onClick={()=>addItem(item)} inverted>ADD TO CART</CustomButton>
     </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchtoProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchtoProps)(CollectionItem);
