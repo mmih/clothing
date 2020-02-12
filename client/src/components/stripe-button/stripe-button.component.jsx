7143a2f3a5c3682fe7dd9f93bfdf31704c5bc469
joelynnturner@gmail.com <joelynnturner@gmail.com>
2020-02-12 21:52:36 +0200

    Updates:
    Moves all client related code to client folder
    
    Adds:
    
      - node server and related packages
      - stripe integration to send data to the server

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_Yg49ILpADlzjWba8jo4edmzD003wljBphS';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment Error:', error);
+      alert('There was an issue with your payment');
+    })
+  }
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='MVM clothing'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+};
+
+export default StripeCheckoutButton;
